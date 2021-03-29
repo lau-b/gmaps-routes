6090a23eec20f743cffd06a07594e848f5aa0205
Laurin Brinkmann <laurin.brinkmann@gmail.com>
2021-03-30 01:13:03 +0200

    brings routes.py in a proper format

--- routes.py
+++ routes.py
@@ -1,51 +1,95 @@
-import folium
-from geojson import Feature, FeatureCollection, LineString
 import os
 import requests
 import json
 from dotenv import load_dotenv
+from geojson import Feature, FeatureCollection, LineString
 
 
-load_dotenv('./.env')
-key = os.getenv('api_key')
-endpoints = []  # contains the start and endpoint of a route (maps call)
-routes = []  # contains n geo-values which describe the route in detail (maps response)
-with open('data/bike_routes.csv', 'r') as file:
-    for line in file:
-        endpoints.append(line.split(','))
+def call_directions_api(start_lat, start_lng, end_lat, end_lng):
+    """
+    Calls the Google Maps Directions API with longitude and latitude values
+    for the starting location and destination.
 
-len(endpoints)
+    Returns a list of every waypoint of the calculated route including passed
+    start_lng and start_lat as first values of that list. end_lng and end_lat
+    might differ as the last values of the route are taken from the response.
 
-routes = []
-for endpoint in endpoints[:15]:
-    url = f'https://maps.googleapis.com/maps/api/directions/json?origin={endpoint[2]},{endpoint[1]}&destination={endpoint[5]},{endpoint[4]}&avoid=highways&mode=bicycling&key={key}'
+    The Directions API might respond with more than one route, if there are
+    alternative routes available. In this care the first will be returned.
+
+    Also, since this function is used to calucate bike routes, additional
+    paramenters are set. Those are:
+    - avoid: highways and ferries (duh!)
+    - mode: bicycling
+
+    :start_lng: float
+    :start_lat: float
+    :end_lng: float
+    :end_lat: float
+    :resp_form: Accepted values xml and json. Default is json.
+    """
+
+    # set defaults. TODO: ist das mit dem API key hier richtig, oder pass?
+    load_dotenv('./.env')
+    key = os.getenv('api_key')
+    avoid = 'highways|ferries'
+    mode = 'bicycling'
+    url = f'https://maps.googleapis.com/maps/api/directions/json?origin={start_lat},{start_lng}&destination={end_lat},{end_lng}&avoid={avoid}&mode={mode}&key={key}'
     resp = requests.get(url)
     # # TODO: if status_code == 200 oder sowas hier einbauen
     json_text = resp.json()
-    steps = json_text['routes'][0]['legs'][0]['steps']
     route = []
-    route.append([float(endpoint[1]), float(endpoint[2])])
+    # Kinda confusing: API Calls expects (lat,lng). GeoJSON does [lng,lat] :(
+    route.append([start_lng, start_lat])
+    steps = json_text['routes'][0]['legs'][0]['steps']
     for step in steps:
         lat = step['end_location']['lat']
         lng = step['end_location']['lng']
         coord = [lng, lat]
         route.append(coord)
 
-    routes.append(route)
+    return route
 
-# %%
-feature_list = []
-for route in routes:
-    feature = Feature(geometry=LineString(route))
-    feature_list.append(feature)
 
-feature_collection = FeatureCollection(feature_list)
-feature_collection
+def read_routes_csv(path):
+    file = []
+    with open(path, 'r') as f:
+        for line in f:
+            file.append(line.split(','))
+    return file
 
-map = folium.Map(
-    location=[50.9411228, 6.9409058],
-    zoom_start=11.5,
-    tiles='Stamen Toner',
-)
-folium.GeoJson(feature_collection, name='pew').add_to(map)
-map
+
+def create_geojson_line(route):
+    """
+    Converts a list of coordinates into a geoJSON Feature
+    """
+    return Feature(geometry=LineString(route))
+
+
+def create_feature_collection(feature_list):
+    return FeatureCollection(feature_list)
+
+
+if __name__ == '__main__':
+    load_dotenv('./.env')
+    key = os.getenv('api_key')
+
+    # this file contains starting and destination coordinates
+    bike_routes = read_routes_csv('data/bike_routes.csv')
+    routes_with_waypoints = []
+    for bike_route in bike_routes[:15]:  # safety net for not calling 55k times
+        routes_with_waypoints.append(call_directions_api(
+            start_lat=float(bike_route[2]),
+            start_lng=float(bike_route[1]),
+            end_lat=float(bike_route[5]),
+            end_lng=float(bike_route[4])
+        ))
+
+    feature_list = []
+    for route_with_waypoints in routes_with_waypoints:
+        feature_list.append(create_geojson_line(route_with_waypoints))
+
+    feature_collection = create_feature_collection(feature_list)
+
+    with open('data/feature_collection.json', 'w') as f:
+        f.write(json.dumps(feature_collection))
